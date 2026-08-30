6ae57dc09d55025704ae51f2a4bfda6b0ac2763c
Shubham Shaswat <98876054+TapItNinja@users.noreply.github.com>
2026-08-30 21:37:47 +0530

    chore: remove apps directory from repo root

--- apps/FinPilot/lib/core/state/app_state_provider.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'app_state_controller.dart';
-
-// final appStateControllerProvider=Provider<AppStateController>((ref){
-//   return AppStateController();
-// });
-//OLD WAY OF DOING NOW USING REACTIVE WAY.
